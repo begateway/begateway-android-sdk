6fe908322a2a5dfa9a971f5f3024e9ce66ff32b4
Alexei Afanasyeu <behemoth-13@yandex.ru>
2021-04-25 21:16:17 +0300

    optimized

--- mobilepayments/src/test/java/com/begateway/mobilepayments/CustomSerializer.kt
+++ mobilepayments/src/test/java/com/begateway/mobilepayments/CustomSerializer.kt
@@ -7,26 +7,22 @@ import java.lang.reflect.Field
 import java.lang.reflect.Type
 
 class CustomSerializer<T : AdditionalFields> : JsonSerializer<T> {
-    private val gson = Gson()
     override fun serialize(
         src: T,
         typeOfSrc: Type,
         context: JsonSerializationContext
     ): JsonElement {
-        val jsonObject = gson.toJsonTree(src) as JsonObject
+        val jsonObject = JsonObject()
         src::class.java.declaredFields.forEach { field ->
-            if (AdditionalFields::class.java.isAssignableFrom(field.type)) {
-                val serializedName = field.getAnnotation(SerializedName::class.java).value
-                jsonObject.remove(serializedName)
-                field.isAccessible = true
-                jsonObject.add(serializedName, context.serialize(field[src]))
-            }
+            val serializedName = field.getAnnotation(SerializedName::class.java).value
+            field.isAccessible = true
+            jsonObject.add(serializedName, context.serialize(field[src]))
         }
-        for (customElement in src.fields) {
-            for ((key, value) in customElement.entrySet()) {
-                jsonObject.add(key, value)
+        src.fields
+            .map { it.entrySet() }
+            .forEach { entrySet ->
+                entrySet.forEach { (key, value) -> jsonObject.add(key, value) }
             }
-        }
         return jsonObject
     }
 }
